5597d3126d9846c890ce3d6717065830a5e8764d
Chitransh900 <chituu900@gmail.com>
2026-05-28 20:51:04 +0530

    Hide Firebase API keys in .env

--- config/firebase.js
+++ config/firebase.js
@@ -19,12 +19,12 @@ import { getFirestore } from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAxlN4h0kEGv5CGw9YquaBAH5qr6A5h8Ow',
-  authDomain: 'tracker-c1dde.firebaseapp.com',
-  projectId: 'tracker-c1dde',
-  storageBucket: 'tracker-c1dde.firebasestorage.app',
-  messagingSenderId: '320165470314',
-  appId: '1:320165470314:android:d30e72a86ea27ca90daa13',
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 import { Platform } from 'react-native';
